611260c709c0a5e7523fc4f450ae88c1672b0378
Poncho <imejia@st-mx.com>
2016-02-16 17:06:41 -0600

    no se que es lo que hice
    Signed-off-by: Poncho <imejia@st-mx.com>

--- scr/Naturaleza/ClsCosmos.h
+++ scr/Naturaleza/ClsCosmos.h
@@ -10,30 +10,29 @@
 #include "singleton_implementacion.h"
 #include "entidadinicializable.h"
 namespace Ciencia {
-namespace Cosmos {
-class Cls_Cosmos : public entidad_inicializable {
-//this is going to be a initializable class
+	namespace Cosmos {
 
-//SINGLETON_DEFINITION(Cls_Cosmos)
+			class Cls_Cosmos : public entidad_inicializable {
+					//this is going to be a initializable class
 
-private:
-Cls_Cosmos();
-static Cls_Cosmos *Instance_;
-public:
-static Cls_Cosmos * GetInstance(void)
-{
-	if (Cls_Cosmos::Instance_ == NULL) Cls_Cosmos::Instance_ = new Cls_Cosmos();
-	return Cls_Cosmos::Instance_ ;
-}
+					//SINGLETON_DEFINITION(Cls_Cosmos)
 
+			private:
+				Cls_Cosmos();
+				static Cls_Cosmos *Instance_;
+			public:
+				static Cls_Cosmos * GetInstance(void)
+				{
+					if (Cls_Cosmos::Instance_ == NULL) Cls_Cosmos::Instance_ = new Cls_Cosmos();
+					return Cls_Cosmos::Instance_ ;
+				}
 /*
 public:
 	Cls_Cosmos();
 */
+			virtual ~Cls_Cosmos();
+			};
 
-	virtual ~Cls_Cosmos();
-};
-
-} /* namespace Cosmos */
+	} /* namespace Cosmos */
 } /* namespace Ciencia */
 #endif /* NATURALEZA_CLSCOSMOS_H_ */
